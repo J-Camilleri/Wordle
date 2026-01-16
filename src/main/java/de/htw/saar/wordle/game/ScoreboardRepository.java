603c7dc46a60faa5cd47adbf8ec4a436d7558c9c
Alexander Pohl <pohltimoalexander260@gmail.com>
2026-01-16 12:24:03 +0100

    Methode um Scoreboard zu erstellen. MUSS vor save() Methode in UserRepository gelaufen sein, da dort änderungen vorgenommen werden. Am besten in DatabaseManager eine initi() Methode schreiben die alle Tables einmal beim start der main initialisiert.

---
+++ src/main/java/de/htw/saar/wordle/game/ScoreboardRepository.java
@@ -0,0 +1,27 @@
+package de.htw.saar.wordle.game;
+
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class ScoreboardRepository {
+
+    public static void createScoreboard() throws SQLException {
+        String createScoreboard = """
+                CREATE TABLE IF NOT EXISTS scoreboard (
+                   user_id INTEGER PRIMARY KEY,
+                   score INTEGER NOT NULL DEFAULT 0,
+                   FOREIGN KEY (user_id) REFERENCES users(id)
+                );
+                """;
+
+        try (Connection con = DatabaseManager.connect();
+             Statement st = con.createStatement()){
+            st.execute(createScoreboard);
+        }
+    }
+
+    public static void
+
+
+
+}
